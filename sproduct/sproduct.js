93cb6fc47154938995bbd67f1a2dcc4f702e7bb4
claudiahW <murungiclaud@gmail.com>
2024-07-29 16:31:24 +0300

    make product page responsive

--- sproduct/sproduct.js
+++ sproduct/sproduct.js
@@ -1,2 +1,16 @@
-var MainImg = document.getElementById("MainImg");
-var smallimg = document.getElementsByClassName("small-img");
+
+var mainImg = document.getElementById("mainImg");
+var smallImg = document.getElementsByClassName("tees");
+
+smallImg[0].onclick = function(){
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function(){
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function(){
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function(){
+  mainImg.src = smallImg[3].src;
+};
